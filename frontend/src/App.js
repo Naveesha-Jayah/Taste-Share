c7963d2de680ae5cf476085cb4123344f179e17f
Naveesha Jayah <175933977+Naveesha-Jayah@users.noreply.github.com>
2025-04-26 16:37:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,35 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import RecipeAdd from "./Components/RecipeManagement/RecipeAdd";
-import CookingPlan from "./Components/CookingPlan/CookingPlan"
-import CreatePlan from "./Components/CookingPlan/CreatePlan"
-import CreateChallenge from "./Components/Challenges/createChallenge"
-import RecipeAdd from "./Components/RecipeManagement/RecipeAdd";
-import CookingPlan from "./Components/CookingPlan/CookingPlan"
-import CreatePlan from "./Components/CookingPlan/CreatePlan"
-import UserLogin from "./Components/UserM/UserLogin"
-import UserRegistration from "./Components/UserM/UserRegistration"
-
+import CookingPlan from "./Components/CookingPlan/CookingPlan";
+import CreatePlan from "./Components/CookingPlan/CreatePlan";
+import CreateChallenge from "./Components/Challenges/createChallenge";
+import UserLogin from "./Components/UserM/UserLogin";
+import UserRegistration from "./Components/UserM/UserRegistration";
 
 function App() {
   return (
-    <div>
-      <React.Fragment>
-        <Routes>
-          <Route path="/" element={<Home />}
-          <Route path="/addrecipe" element={<RecipeAdd />} />
-          <Route path="/cooking-plan" element={<CookingPlan />} />
-          <Route path="/create-plan" element={<CreatePlan />} />
-          <Route path="/create-challenge" element={<CreateChallenge />} />
-          <Route path="/addrecipe" element={<RecipeAdd />} />
-          <Route path="/cooking-plan" element={<CookingPlan />} />
-          <Route path="/create-plan" element={<CreatePlan />} />
-          <Route path="/user-login" element={<UserLogin />} />
-          <Route path="/user-register" element={<UserRegistration />} />
-
-        </Routes>
-      </React.Fragment>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addrecipe" element={<RecipeAdd />} />
+        <Route path="/cooking-plan" element={<CookingPlan />} />
+        <Route path="/create-plan" element={<CreatePlan />} />
+        <Route path="/create-challenge" element={<CreateChallenge />} />
+        <Route path="/user-login" element={<UserLogin />} />
+        <Route path="/user-register" element={<UserRegistration />} />
+      </Routes>
+    </Router>
   );
 }
 

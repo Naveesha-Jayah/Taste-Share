733d555c63415887347ccb526f8fdc45e96955b9
Naveesha Jayah <173625012+Kavinav@users.noreply.github.com>
2025-04-26 16:26:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import Register from "./Components/UserManagement/Registration";
-import  Login from "./Components/UserManagement/Login";
-import Profile from "./Components/UserManagement/UserProfile";
 import RecipeAdd from "./Components/RecipeManagement/RecipeAdd";
 import CookingPlan from "./Components/CookingPlan/CookingPlan"
 import CreatePlan from "./Components/CookingPlan/CreatePlan"
@@ -15,9 +12,6 @@ function App() {
       <React.Fragment>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/addrecipe" element={<RecipeAdd />} />
           <Route path="/cooking-plan" element={<CookingPlan />} />
           <Route path="/create-plan" element={<CreatePlan />} />
